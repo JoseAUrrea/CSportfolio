44a4e0ae57dc40eb37c380b46f446f02fd4b750a
JoseAUrrea <joseurrea@knights.ucf.edu>
2023-12-12 01:03:04 -0500

    output in config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    basePath: '/csportfolio',
-    assetPrefix: '/csportfolio/',
+    output: 'export'
   }
   
   module.exports = nextConfig;
