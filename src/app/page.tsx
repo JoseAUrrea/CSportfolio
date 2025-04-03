9bfe3a3c68f1a704a852df60689baeda73ea9836
JoseAUrrea <alejandro@altercall.com>
2025-04-03 14:29:17 -0700

    responsiveness on header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,11 +35,11 @@ export default function Home() {
             <div className='flex flex-col justify-center items-center '>
             <h1 className="text-[40px] font-bold">
             </h1>
-            <h1 className="text-white text-[35px] font-bold animate-slideIn bg-gradient-to-l from-cyan-500 to-blue-500 bg-gray-200 px-6 py-1 rounded-full">
+            <h1 className="text-white text-[30px] md:text-[35px] font-bold animate-slideIn bg-gradient-to-l from-cyan-500 to-blue-500 bg-gray-200 px-6 py-1 rounded-full">
               Jose Alejandro Urrea
             </h1>
             </div>
-            <p className="flex items-center text-[30px] mb-4">
+            <p className="flex items-center text-[26px] md:text-[30px] mb-4">
               <span>Full Stack Engineer</span>
               <AiTwotoneCode className="ml-2 inline-block" size={30} />
             </p>
