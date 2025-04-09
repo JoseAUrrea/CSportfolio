72019b08b87c6d9d168467f1471936de61919143
JoseAUrrea <alejandro@altercall.com>
2025-04-09 09:50:17 -0700

    updates

--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, Image, CardFooter} from "@nextui-org/react";
 import Link from "next/link";
 
 const Projects = () => {
@@ -10,25 +10,27 @@ const Projects = () => {
             </h1>
             <p className="pb-4">Click on the titles to reach the repository</p>
             <div className="grid xl:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-6 md:pr-6 pb-16 border-b border-gray-300">
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-                    <p className="text-tiny uppercase font-bold">Compete</p>
-                    <h4 className="font-bold text-large">A virtual sports & social media app built for a client</h4>
-                    <small className="text-default-500">NextJS, React, Typescript, Tailwind CSS, MySQL, & AWS</small>
-                    <small className="text-default-500">My Roles: Database & Backend</small>
-                    <small className="text-default-500">* Do not own access to repository</small>
+                    <p className="text-[20px] uppercase font-bold">Compete</p>
+                    <p className="font-bold text-[18px]">Virtual Sports & Social Media App</p>
+                    <small className="text-[16px]"> Database & Backend</small>
+                    <small className="text-[16px]">NextJS, React, Typescript, Tailwind CSS, MySQL, & AWS</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/compete.jpeg"
                     width={400}
                     />
                 </CardBody>
+                <CardFooter>
+                    <small className="text-[16px] px-4">* Private Repository</small>
+                </CardFooter>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -36,21 +38,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">The Market</p>
                     </a>
-                    <h4 className="font-bold text-large">An ecommerce site to shop for what you need</h4>
-                    <small className="text-default-500">Python, Django, HTML, SQLite, CSS, & JavaScript</small>
-                    <small className="text-default-500">My Roles: Full Stack</small>
+                    <p className="font-bold text-[18px]">Ecommerce Website</p>
+                    <small className="text-[16px]"> Full Stack</small>
+                    <small className="text-[16px]">Python, Django, HTML, SQLite, CSS, & JavaScript</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/ecommerce.jpg"
                     width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -58,21 +60,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Ocala Equine Experience</p>
                     </a>
-                    <h4 className="font-bold text-large">Book your perfect stay with this website</h4>
-                    <small className="text-default-500">HTML, CSS, Javascript, Bootstrap</small>
-                    <small className="text-default-500">My Role: Frontend</small>
+                    <p className="font-bold text-[18px]">Booking Website</p>
+                    <small className="text-[16px]">Frontend</small>
+                    <small className="text-[16px]">HTML, CSS, Javascript, Bootstrap</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/equine.jpg"
-                    width={375}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -80,21 +82,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Personal Trainer GPT</p>
                     </a>
-                    <h4 className="font-bold text-large">An AI personal trainer to chat with</h4>
-                    <small className="text-default-500">MERN stack</small>
-                    <small className="text-default-500">My Role: Database</small>
+                    <p className="font-bold text-[18px]">AI Personal Trainer</p>
+                    <small className="text-[16px]">Backend & Database</small>
+                    <small className="text-[16px]">MERN stack</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/pt-gpt2.jpeg"
-                    width={300}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -102,21 +104,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Puzzles</p>
                     </a>
-                    <h4 className="font-bold text-large">A draggable and sliding puzzle</h4>
-                    <small className="text-default-500">HTML, CSS, Javascript, JQuery </small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">A draggable and sliding puzzle</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">HTML, CSS, Javascript, JQuery </small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/puzzle.jpg"
-                    width={350}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -124,21 +126,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Canny Edge Detector</p>
                     </a>
-                    <h4 className="font-bold text-large">A 3 part program to detect edges of an image</h4>
-                    <small className="text-default-500">C - Canny Algorithm</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">Detects edges of an input image</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">C - Canny Algorithm</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/cannypt2.jpg"
-                    width={250}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -146,21 +148,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Bingo</p>
                     </a>
-                    <h4 className="font-bold text-large">An online bingo game</h4>
-                    <small className="text-default-500">React, HTML, CSS, Javascript</small>
-                    <small className="text-default-500">My Role: Individual </small>
+                    <p className="font-bold text-[18px]">An online bingo game</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">React, HTML, CSS, Javascript</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/bingo.jpg"
-                    width={370}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -168,21 +170,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Olympic Medal Predictor</p>
                     </a>
-                    <h4 className="font-bold text-large">Uses data from previous Olympics and predicts how many medals each country will earn at the next Olympics</h4>
-                    <small className="text-default-500">Python - Machine Learning</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">Predicts how many medals each country will earn at the next Olympics</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">Python - Machine Learning</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/medals.jpeg"
-                    width={300}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -190,21 +192,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Web Scraping Soccer Data</p>
                     </a>
-                    <h4 className="font-bold text-large">A program to collect team stats from the past three seasons and convert it into a csv file</h4>
-                    <small className="text-default-500">Python - Web Scraper</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">Collects team stats from the past three seasons and converts it into a csv file</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">Python - Web Scraper</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/webscrape.jpeg"
-                    width={300}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -212,21 +214,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">To Do List</p>
                     </a>
-                    <h4 className="font-bold text-large">A simple to-do list app to keep track of tasks</h4>
-                    <small className="text-default-500">HTML5, CSS, & JavaScript</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">A simple to-do list app to keep track of tasks</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">HTML5, CSS, & JavaScript</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/toDoList.jpg"
-                    width={350}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -234,21 +236,21 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Word Sorting</p>
                     </a>
-                    <h4 className="font-bold text-large">Analyzes the frequency of words in multiple files</h4>
-                    <small className="text-default-500">C - Binary Search Tree</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">Analyzes the frequency of words in files</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">C - Binary Search Tree</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/bst.jpeg"
-                    width={250}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
 
-                <Card className="py-2 px-1 md:w-[400px] w-[350px] bg-gray-600 hover:bg-opacity-40 bg-opacity-25 rounded-lg">
+                <Card className="projectCard">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <a
                         target="_blank"
@@ -256,16 +258,16 @@ const Projects = () => {
                     >
                     <p className="text-tiny uppercase font-bold underline underline-offset-2 hover:text-blue-700">Library Sorting</p>
                     </a>
-                    <h4 className="font-bold text-large">Given the total pages in x amount of books and a maximum number of pages you will read, the maximum number of books you can fully read is displayed</h4>
-                    <small className="text-default-500">C - Merge Sort</small>
-                    <small className="text-default-500">Individual</small>
+                    <p className="font-bold text-[18px]">Given the total pages in x amount of books and a maximum number of pages you will read, the maximum number of books you can fully read is displayed</p>
+                    <small className="text-[16px]">Solo Project </small>
+                    <small className="text-[16px]">C - Merge Sort</small>
                 </CardHeader>
                 <CardBody className="overflow-visible py-2 px-1 items-center">
                     <Image
                     alt="Card background"
-                    className="object-cover rounded-xl"
+                    className="object-cover rounded-md"
                     src="/images/mergeSort.jpeg"
-                    width={300}
+                    width={400}
                     />
                 </CardBody>
                 </Card>
