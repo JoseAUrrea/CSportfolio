6ac092a7aec45952ef3fef1ac6944aceda9299e5
JoseAUrrea <alejandro@altercall.com>
2025-04-09 09:16:03 -0700

    updates

--- src/app/components/Technologies.tsx
+++ src/app/components/Technologies.tsx
@@ -7,7 +7,7 @@ const Technologies = ({ techList }: TechListTypes) => {
     const backendAndDatabases = ["SQL", "MongoDB", "Prisma", "PostgreSQL", "MySQL", "SQLite", "Restful", "ORMs"];
     const devOpsAndCloud = ["AWS", "Azure", "Docker", "Kubernetes", "CI/CD", "Git", "GitHub", "GitLab"];
     const projectDesignAndMethodologies = ["Agile", "Scrum", "Jira", "Trello", "Figma", "Discord", "Slack"];
-    const designAndMultimedia = ["Adobe Photoshop", "Premiere Pro", "Creative Cloud"];
+    const designAndMultimedia = ["Adobe", "Premiere Pro", "Creative Cloud"];
     return (
         <div className="flex flex-col items-center px-4 pt-16 max-w-[1200px]">
             
@@ -15,57 +15,61 @@ const Technologies = ({ techList }: TechListTypes) => {
                 Technologies
             </h1>
             
-            <h2 className="text-[20px]">Programming Languages</h2>
-            <h2 className="text-[20px]">and Frameworks</h2>
-            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-6 pb-16 border-gray-300 w-full"> 
+            <h2 className="text-[20px] pb-4">Programming Languages and Frameworks</h2>
+            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-8 pb-16 border-gray-300 w-full"> 
             {programmingLanguagesAndFrameworks.map((tech, index) => (
-                <Card key={index} className="max-w-[450px] my-2 hover:animate-bounce rounded-2xl px-6 py-2 gradient-bg opacity-80">
+                <Card key={index} className="max-w-[800px] px-4 py-2 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md 
+                    bg-gradient-to-l from-[rgba(6,182,212,0.4)] to-[rgba(59,130,246,0.4)]">
                     <CardBody className="flex items-center justify-center">
-                        <p className = "text-slate-200">{tech}</p>
+                        <p>{tech}</p>
                     </CardBody>
                 </Card>
             ))}
             </div>
             
-            <h2 className="text-[20px]">Backend and Database</h2>
-            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-6 pb-16 border-gray-300 w-full"> 
+            <h2 className="text-[20px] pb-4">Backend and Database</h2>
+            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-8 pb-16 border-gray-300 w-full"> 
             {backendAndDatabases.map((tech, index) => (
-                <Card key={index} className="max-w-[450px] my-2 hover:animate-bounce rounded-2xl px-6 py-2 gradient-bg opacity-80">
+                <Card key={index} className="max-w-[800px] px-4 py-2 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md 
+                    bg-gradient-to-l from-[rgba(6,182,212,0.4)] to-[rgba(59,130,246,0.4)]">
                     <CardBody className="flex items-center justify-center">
-                        <p className = "text-slate-200">{tech}</p>
+                        <p>{tech}</p>
                     </CardBody>
                 </Card>
             ))}
             </div>
             
-            <h2 className="text-[20px]">DevOps and Cloud</h2>
-            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-6 pb-16 border-gray-300 w-full"> 
+            <h2 className="text-[20px] pb-4">DevOps and Cloud</h2>
+            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-8 pb-16 border-gray-300 w-full"> 
             {devOpsAndCloud.map((tech, index) => (
-                <Card key={index} className="max-w-[450px] my-2 hover:animate-bounce rounded-2xl px-6 py-2 gradient-bg opacity-80">
+                <Card key={index} className="max-w-[800px] px-4 py-2 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md 
+                    bg-gradient-to-l from-[rgba(6,182,212,0.4)] to-[rgba(59,130,246,0.4)]">
                     <CardBody className="flex items-center">
-                        <p className = "text-slate-200">{tech}</p>
+                        <p>{tech}</p>
                     </CardBody>
                 </Card>
             ))}
             </div>
 
-            <h2 className="text-[20px]">Project Design and Methodologies</h2>
-            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-6 pb-16 border-gray-300 w-full"> 
+            <h2 className="text-[20px] pb-4">Project Design and Methodologies</h2>
+            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-8 pb-16 border-gray-300 w-full"> 
             {projectDesignAndMethodologies.map((tech, index) => (
-                <Card key={index} className="max-w-[450px] my-2 hover:animate-bounce rounded-2xl px-6 py-2 gradient-bg opacity-80">
+                <Card key={index} className="max-w-[800px] px-4 py-2 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md 
+                    bg-gradient-to-l from-[rgba(6,182,212,0.4)] to-[rgba(59,130,246,0.4)]">
                     <CardBody className="flex items-center">
-                        <p className = "text-slate-200">{tech}</p>
+                        <p>{tech}</p>
                     </CardBody>
                 </Card>
             ))}
             </div>
 
-            <h2 className="text-[20px]">Design and Multimedia</h2>
-            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-6 pb-16 border-b border-gray-300 w-full"> 
+            <h2 className="text-[20px] pb-4">Design and Multimedia</h2>
+            <div className = "grid lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-8 pb-16 border-gray-300 w-full"> 
             {designAndMultimedia.map((tech, index) => (
-                <Card key={index} className="max-w-[450px] my-2 hover:animate-bounce rounded-2xl px-6 py-2 gradient-bg opacity-80">
+                <Card key={index} className="max-w-[800px] px-4 py-2 rounded-2xl bg-clip-padding backdrop-filter backdrop-blur-md 
+                    bg-gradient-to-l from-[rgba(6,182,212,0.4)] to-[rgba(59,130,246,0.4)]">
                     <CardBody className="flex items-center">
-                        <p className = "text-slate-200">{tech}</p>
+                        <p>{tech}</p>
                     </CardBody>
                 </Card>
             ))}
